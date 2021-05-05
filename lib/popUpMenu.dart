9abfb5299e732044971412e46e34b31ed4f26047
muazamhussain7 <muazam.hussain@venturedive.com>
2021-05-05 14:47:13 +0500

    Added highlight color for selected popUpMenuItem.

--- lib/popUpMenu.dart
+++ lib/popUpMenu.dart
@@ -3,17 +3,30 @@ import 'package:provider/provider.dart';
 
 import 'todos.dart';
 
-class PopUpMenu extends StatelessWidget {
+class PopUpMenu extends StatefulWidget {
+  @override
+  _PopUpMenuState createState() => _PopUpMenuState();
+}
+
+class _PopUpMenuState extends State<PopUpMenu> {
+  var _selectedMenuItem = 0;
   @override
   Widget build(BuildContext context) {
-    final todoContainer = Provider.of<Todos>(context);
+    final todoContainer = Provider.of<Todos>(context, listen: false);
     return PopupMenuButton(
       icon: Icon(Icons.more_vert),
+      initialValue: _selectedMenuItem,
       onSelected: (int selectedValue) {
         if (selectedValue == 0) {
           todoContainer.showPending();
+          setState(() {
+            _selectedMenuItem = 0;
+          });
         } else {
           todoContainer.showCompleted();
+          setState(() {
+            _selectedMenuItem = 1;
+          });
         }
       },
       itemBuilder: (_) => [
